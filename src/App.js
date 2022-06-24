3b8d4b20cc7fc14bff2a8eebaa01a24fe3f880cb
T A Comer <tacomer@gmail.com>
2022-06-24 11:48:14 -0400

    Map working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
-import Test from "./components/Map/Test";
-import { Helmet } from 'react-helmet';
+import Map from "./components/Map";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <div className="App">
       <Helmet>
         <title>Trailmap V4</title>
-        <link href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.2.0/mapbox-gl-geocoder.css' rel='stylesheet' />
-      </Helmet>      
-      <Test />
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.2.0/mapbox-gl-geocoder.css"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Map />
     </div>
   );
 };
